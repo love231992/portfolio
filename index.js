47859808a29dd745975c5b09b85e323de47f3519
iamlove <love231992@gmail.com>
2020-04-26 18:39:10 +0530

    media queries

--- index.js
+++ index.js
@@ -48,3 +48,24 @@ const showRequiredCategory = event => {
     getCategory.classList.remove("hideCategory")
     getCategory.classList.add('showCategory')
 }
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((links,index) => {
+            if (links.style.animation) {
+                links.style.animation = "";
+            } else {
+            links.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        }
+        });
+        burger.classList.toggle('toggle');
+    })
+    
+}
+
+navSlide();
